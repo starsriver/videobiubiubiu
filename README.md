95d98f2c469bcc54325b7259b935fbcd06eaa75a
starsriver <starsriver@outlook.com>
2016-02-10 21:18:03 +0800

    初始化

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # videobiubiubiu
+# 是一个个人实验性质的HTML5网页弹幕播放器
---
+++ main.js
@@ -0,0 +1,19 @@
+﻿function init()
+{
+    var mainvideo=document.getElementById("main_video");
+    var maincanvas=document.getElementById("main_canvas");
+    var Width=mainvideo.videoWidth;
+    var Height=mainvideo.videoHeight;
+    maincanvas.width=Width;
+    maincanvas.height=Height-40;
+    console.log(Width);
+    console.log(Height);
+}
+function playORpause()
+{
+    var mainvideo=document.getElementById("main_video");
+    if(mainvideo.paused===false)
+        mainvideo.pause();
+    else
+        mainvideo.play();
+}
